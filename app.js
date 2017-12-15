fe1cd57ce05f657164ecc49dcd1914f28e733a2b
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-12-15 10:29:42 -0500

    dirname build

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.get('/', (req, res) => {
-  res.sendFile('/client/build/index.html')
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
 app.use('*', (req, res) => {
